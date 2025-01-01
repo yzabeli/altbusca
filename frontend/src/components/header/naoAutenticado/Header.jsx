bde1cac634ab2bb7944d61b59a659b0fb3a13de4
monosodrac <monosodrac@gmail.com>
2025-01-01 16:31:46 -0300

    remove a área de busca

--- frontend/src/components/header/naoAutenticado/Header.jsx
+++ frontend/src/components/header/naoAutenticado/Header.jsx
@@ -1,50 +1,18 @@
-import React, { useState, useMemo } from 'react';
+import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
-import Form from 'react-bootstrap/Form';
-import InputGroup from 'react-bootstrap/InputGroup';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Person } from 'react-bootstrap-icons';
 
-import { todos } from '../../../assets/assets';
 import '../Header.css';
 
 export default function Header() {
-    const [busca, setBusca] = useState('');
-
-    const filtrados = useMemo(() => {
-        const lowerBusca = busca.toLocaleLowerCase();
-        return todos.filter((item) =>
-            item.nome.toLocaleLowerCase().includes(lowerBusca));
-    }, [busca]);
-
     return (
         <Navbar expand="lg" sticky="top" className="p-3" id="cabecalho">
             <Container className='d-lg-flex d-md-block d-sm-block'>
                 <div className="logo__principal">
                     <Navbar.Brand href='/' className="m-0 section-title">Alt Busca&#128375;</Navbar.Brand>
                 </div>
-                <div className="area__buscar">
-                    <div className="input__busca">
-                        <InputGroup>
-                            <Form.Control
-                                placeholder="Digite sua busca..."
-                                aria-label="Busca"
-                                aria-describedby="basic-addon1"
-                                value={busca}
-                                onChange={(e) => setBusca(e.target.value)}
-                            />
-                        </InputGroup>
-                    </div>
-                    {busca && (
-                        <ul id='listaProds'>
-                            {filtrados.map((item, index) => (
-                                <li key={index}>
-                                    <a href="/">{item.nome}</a>
-                                </li>
-                            ))}
-                        </ul>
-                    )}
-                </div>
+                
                 <div className="menu__navbar">
                     <Nav>
                         <Nav.Link href="/cadastro" className="fw-bold me-2">
